944931bd139f720476ca99b3bd3970f691d30d52
jancases <97055897+jancases@users.noreply.github.com>
2022-01-07 15:54:50 -0500

    Update MoneyChangerTests.java

--- src/test/java/boot/apps/helper/MoneyChangerTests.java
+++ src/test/java/boot/apps/helper/MoneyChangerTests.java
@@ -1,5 +1,7 @@
 package boot.apps.helper;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -17,6 +19,26 @@ public class MoneyChangerTests {
 		Assertions.assertTrue(moneyChangerHelper.isValidBill(20));
 		Assertions.assertTrue(moneyChangerHelper.isValidBill(50));
 		Assertions.assertTrue(moneyChangerHelper.isValidBill(100));
+	}
+	
+	AtomicInteger quartersAlloc = new AtomicInteger(10);
+	AtomicInteger dimesAlloc 	= new AtomicInteger(10);
+	AtomicInteger nickelsAlloc 	= new AtomicInteger(10);
+	AtomicInteger penniesAlloc 	= new AtomicInteger(10);
+	
+	@Test
+	void validAllocation() {
+		
+		MoneyChangerHelper moneyChangerHelper = new MoneyChangerHelper();
+		
+		Assertions.assertTrue(moneyChangerHelper.isAllocationEnough(1, quartersAlloc, dimesAlloc, nickelsAlloc, penniesAlloc));
+		Assertions.assertTrue(moneyChangerHelper.isAllocationEnough(2, quartersAlloc, dimesAlloc, nickelsAlloc, penniesAlloc));
+		Assertions.assertTrue(moneyChangerHelper.isAllocationEnough(5, quartersAlloc, dimesAlloc, nickelsAlloc, penniesAlloc));
+		Assertions.assertTrue(moneyChangerHelper.isAllocationEnough(10, quartersAlloc, dimesAlloc, nickelsAlloc, penniesAlloc));
+		Assertions.assertTrue(moneyChangerHelper.isAllocationEnough(20, quartersAlloc, dimesAlloc, nickelsAlloc, penniesAlloc));
+		Assertions.assertTrue(moneyChangerHelper.isAllocationEnough(50, quartersAlloc, dimesAlloc, nickelsAlloc, penniesAlloc));
+		Assertions.assertTrue(moneyChangerHelper.isAllocationEnough(100, quartersAlloc, dimesAlloc, nickelsAlloc, penniesAlloc));
+		
 	}
 
 }
